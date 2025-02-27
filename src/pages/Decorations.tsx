2212cd7b0501c48e9169d565d87afac6e7925db7
Dilshan Prasanna Allepola <110511686+ALLEPOLA@users.noreply.github.com>
2025-02-28 02:00:27 +0530

    page 1

---
+++ src/pages/Decorations.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Decorations: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Decorations Page!</h1>
+    </div>
+  );
+};
+
+export default Decorations;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Caterring from "../pages/Catering"
+import Decorations from "../pages/Decorations";
+import EventNames from "../pages/EventNames";
 
 
 const AppRoute = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+       <Route path="/" element={<EventNames />} />
+       <Route path="/catering" element={<Caterring />}/>
+       <Route path="/Decorations" element={<Decorations />}/>
+      
       </Routes>
     </Router>
   );
